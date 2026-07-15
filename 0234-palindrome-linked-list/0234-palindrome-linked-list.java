4a96fd0b44a547b3ba7830ea8bf01f25d9142897
rajzite <159891962+rajzite@users.noreply.github.com>
2026-07-15 14:29:01 +0530

    Time: 11 ms (13.52%), Space: 91.9 MB (99.37%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null) return true;
+        Stack<Integer> s = new Stack<>();
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            s.push(slow.val);
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if(fast != null){
+            slow = slow.next;
+        }
+        while (slow!=null){
+            if (slow.val == s.peek()){
+                s.pop();
+            }else{
+                return false;
+            }
+            slow = slow.next;
+        }
+        return true;
+    }
+}
